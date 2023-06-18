75f7eb431ebddb16800d95d3d029840aa14a423d
biswajeetbehera123 <107670354+biswajeetbehera123@users.noreply.github.com>
2023-06-18 22:54:57 +0530

    header done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import { AppContext } from './context/contextApi';
-import Header from './components/Header';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Feed from './components/Feed'
 import SearchResult from './components/SearchResult'
 import VideoDetails from './components/VideoDetails'
+import Header from './components/Header';
 
 function App() {
   return (
     <AppContext>
-      <div className="text-3xl">App</div>
+      <BrowserRouter>
+        <div className='flex flex-col h-full'>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Feed />} />
+            <Route path='searchResult/:searchQuery' element={<SearchResult />} />
+            <Route path='video/:id' element={<VideoDetails />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </AppContext>
   );
 }
--- src/context/contextApi.js
+++ src/context/contextApi.js
@@ -1,6 +1,7 @@
 import React, { createContext, useState, useEffect } from "react";
 
 import { fetchDataFromApi } from "../utils/api";
+
 export const Context = createContext();
 
 export const AppContext = (props) => {
