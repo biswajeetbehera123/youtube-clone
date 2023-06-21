79430c4acf202b4e353cf2c0dea67c1ea0fbaa1b
biswajeetbehera123 <107670354+biswajeetbehera123@users.noreply.github.com>
2023-06-21 14:09:19 +0530

    input updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -61,6 +61,7 @@ function Header() {
             onChange={(e) => setSearchQuery(e.target.value)}
             onKeyUp={searchQueryHandler}
             value={searchQuery}
+            placeholder={'Search'}
           />
         </div>
         <button className='w-[40px] md:w-[60px] h-8 md:h-10 flex items-center justify-center border border-l-0 border-[#303030] rounded-r-3xl bg-white/[0.1]'>
